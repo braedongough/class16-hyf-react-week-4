64bbe5cd9fbe195bd738991d1b1ff60b23bba6bf
Braedon <braedongough@gmail.com>
2021-03-14 14:56:04 +0100

    init commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import {useTheme} from './ThemeContext'
+import Todos from './Todo'
+import './App.css'
 
 function App() {
+  const themeContext = useTheme()
+  console.log(themeContext)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Todos />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
